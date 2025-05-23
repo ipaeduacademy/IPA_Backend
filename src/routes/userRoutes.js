7fe52b7b2c2b4f963dcca782733b3e9a0177439e
parth <parthgodase123@gmail.com>
2025-05-23 20:37:05 +0530

    user progress duplicate removed

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getUser, updateUserProgress, getUserProgress, addUserProgress } = require('../controllers/userController');
+const { getUser} = require('../controllers/userController');
 const { authenticate } = require('../middlewares/authMiddlewares');
-const { userProgressSchema } = require('../schemas/userSchemas');
-const { validate } = require('../middlewares/validationMiddlewares');
+
 
 router.get('/getUser', authenticate, getUser);
-router.post('/addUserProgress', authenticate, validate(userProgressSchema), addUserProgress);
-router.put('/updateUserProgress', authenticate, validate(userProgressSchema), updateUserProgress);
-router.get('/getUserProgress', authenticate, getUserProgress);
 
 module.exports = router;
